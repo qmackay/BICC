f84928073984605b7df3e6be3e238b20dc772767
Quinn Mackay <130604060+qmackay@users.noreply.github.com>
2025-02-04 21:36:25 -0800

    Fixing intervals code

--- Python Code/interval_generator.py
+++ Python Code/interval_generator.py
@@ -5,22 +5,27 @@ import numpy as np
 agedepth_file = "E:\GitHub\BICC\Paleochrono\EDML-WDC_test\WDC\WD_AgeDepth.txt"
 df = pd.read_csv(agedepth_file, delimiter="\t", comment="#", names=["depth", "age", "sigma"])
 
-# Define 1000-year intervals
+# Define year intervals
 min_age = df["age"].min()
 max_age = df["age"].max()
-intervals = np.arange(min_age, max_age, 1000)
+intervals = np.arange(min_age, max_age, 100) #set year intervals, ignoring the end if less than 100 years
 
 # Interpolate depths for the defined age intervals. (this works because the original file doesn't skip depths, so doesn't need to interpolate)
 depths = np.interp(intervals, df["age"], df["depth"])
 
+sigma_start = np.interp(intervals, df["age"], df["sigma"])
+sigma_end = np.interp(intervals+99, df["age"], df["sigma"])
+
+sigma_diff = sigma_end - sigma_start
+
 interval_df = pd.DataFrame({
     "top depth (m)": depths[:-1],
     "bottom depth (m)": depths[1:],
-    "duration (yr)": 1000,
-    "sigma duration (yr)": 50 #how should I calculate this?
+    "duration (yr)": 100,
+    "sigma duration (yr)": sigma_diff[:-1] #how should I calculate this?
 })
 
-output_file = "E:\GitHub\BICC\Paleochrono\EDML-WDC_test\WDC\WD_AgeDepth_intervals.txt"
+output_file = "E:\GitHub\BICC\Paleochrono\EDML-WDC_test\WDC\ice_age_intervals.txt"
 interval_df.to_csv(output_file, sep="\t", index=False)
 
 print(f"Done! Saved to: {output_file}")
